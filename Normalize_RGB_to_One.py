82303ddb0945aec77047f799d096d56ff10ca4fd
DCoss1995 <83833508+DCoss1995@users.noreply.github.com>
2024-12-10 13:42:23 +0100

    Update Normalize_RGB_to_One.py

--- Normalize_RGB_to_One.py
+++ Normalize_RGB_to_One.py
@@ -9,13 +9,13 @@ Use when you need to normalize the RGB triplets from 255 to 1.
 
 """
 
-while True:
-    num1 = int(input("Type the first (red channel) number: "))
-    num2 = int(input("Type the second (green channel) number: "))
-    num3 = int(input("Type the third (blue channel) number: "))
+while True: # Allows to keep the script going until the user press two times enter.
+    num1 = int(input("Type the first (red channel) number: ")) # Type the red channel number (0-255)
+    num2 = int(input("Type the second (green channel) number: ")) # Type the green channel number (0-255)
+    num3 = int(input("Type the third (blue channel) number: ")) # Type the blue channel number (0-255)
 
-    print("The normalized red value is: ", num1/255)
-    print("The normalized green value is: ", num2/255)
-    print("The normalized blue value is: ", num3/255)
+    print("The normalized red value is: ", num1/255) # Return the red value normalized to 1.
+    print("The normalized green value is: ", num2/255) # Return the green value normalized to 1.
+    print("The normalized blue value is: ", num3/255) # Return the blue value normalized to 1.
     print("If you want to compute another RGB triplet, keep going. Press enter if you want to end the script.")
-input()
+input() # Pressing enter, the user quits the script.
